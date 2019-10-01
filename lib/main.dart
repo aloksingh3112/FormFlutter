b3de658eacd2c3733567c38dae1ba052f1d5f87b
aloksingh3112 <aloksingh3112@gmail.com>
2019-10-01 20:20:26 +0530

    added form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,13 +27,63 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> _formkey = new GlobalKey<FormState>();
+  List<String> _colors = ['', 'red', 'green', 'orange'];
+  String _color = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: new SafeArea(
+        top: false,
+        bottom: false,
+        child: new Form(
+          key: _formkey,
+          autovalidate: true,
+          child: new ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            children: <Widget>[
+              new TextFormField(
+                decoration: const InputDecoration(
+                    icon: const Icon(Icons.person),
+                    hintText: 'Enter Your First name',
+                    labelText: 'Name'),
+              ),
+              new TextFormField(
+                decoration: const InputDecoration(
+                    icon: const Icon(Icons.calendar_today),
+                    hintText: 'Enter Your Date of Birth',
+                    labelText: 'Date of Birth'),
+                keyboardType: TextInputType.datetime,
+              ),
+              new TextFormField(
+                decoration: const InputDecoration(
+                    icon: const Icon(Icons.phone),
+                    hintText: 'Énter your Phone number',
+                    labelText: 'Phone'),
+                keyboardType: TextInputType.phone,
+                inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+              ),
+              new TextFormField(
+                decoration: const InputDecoration(
+                    icon: const Icon(Icons.email),
+                    hintText: 'Enter your Email',
+                    labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              new Container(
+                padding: const EdgeInsets.only(left: 40.0, top: 20.0),
+                child: new RaisedButton(
+                  child: const Text('Submit'),
+                  onPressed: null,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
